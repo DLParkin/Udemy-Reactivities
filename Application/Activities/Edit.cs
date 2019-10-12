9d2d7da54dc860fbcbf8c345df0338d4337f71f6
David <davidsatwork2@hotmail.com>
2019-10-12 12:44:33 +0800

    add edit logic to Edit.cs

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -10,6 +10,21 @@ namespace Application.Activities
     {
         public class Command : IRequest
         {
+            // need for access to specific activity
+            public Guid Id { get; set; }
+
+            // allow user to edit
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+
+            public string Category { get; set; }
+
+            public DateTime? Date { get; set; }
+
+            public string City { get; set; }
+
+            public string Venue { get; set; }
         }
 
         public class Handler : IRequestHandler<Command>
@@ -23,7 +38,20 @@ namespace Application.Activities
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
-                //handler logic
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                {
+                    throw new Exception("Could not find activity");
+                }
+
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+
                 var success = await _context.SaveChangesAsync() > 0;
 
                 if (success) return Unit.Value;
