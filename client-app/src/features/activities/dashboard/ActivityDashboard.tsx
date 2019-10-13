fda96e69a962c89fd7a448caa43aed10bd83a242
David <davidsatwork2@hotmail.com>
2019-10-13 18:12:57 +0800

    flesh out activity list item

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, List } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
+import ActivityList from './ActivityList';
 
 interface IProps {
   activities: IActivity[];
@@ -10,11 +11,13 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <List>
+        {/* <List>
           {activities.map(activity => (
-            <List.Item key={activity.id}>{activity.title}</List.Item>
+            // <List.Item key={activity.id}>{activity.title}</List.Item>
+            <ActivityList activity={activity}/>
           ))}
-        </List>
+        </List> */}
+        <ActivityList activities={activities}/>
       </Grid.Column>
     </Grid>
   );
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Image, Item, Button, Label, Segment } from 'semantic-ui-react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
 
-const ActivityList = () => {
+interface IProps {
+  activities: IActivity[];
+}
+
+const ActivityList: React.FC<IProps> = ({ activities }) => {
   return (
     <Segment clearing>
       <Item.Group divided>
-        <Item>
-          <Item.Content>
-            <Item.Header as="a">title</Item.Header>
-            <Item.Meta>date</Item.Meta>
-            <Item.Description>
-              <div>Description</div>
-              <div>city, venue</div>
-            </Item.Description>
-            <Item.Extra>
-              <Button floated="right" content="view" color="blue" />
-            </Item.Extra>
-            <Label basic content="Category" />
-          </Item.Content>
-        </Item>
+        {activities.map(activity => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.venue} , {activity.city}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button floated="right" content="view" color="blue" />
+              </Item.Extra>
+              <Label basic content="Category" />
+            </Item.Content>
+          </Item>
+        ))}
       </Item.Group>
     </Segment>
   );
