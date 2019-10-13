2e4581e3d95f69932cdaeb99a38770ade900e98e
David <davidsatwork2@hotmail.com>
2019-10-13 13:17:27 +0800

    change componentdidmount to useeffect for activities response

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Header, Icon, List } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
-  // componentDidMount() {
-  //   axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
-  //     this.setState({
-  //       activities: response.data
-  //     });
-  //   });
-  // }
+
+  useEffect(() => {
+    axios.get<IActivity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    });
+  });
 
   return (
     <div>
