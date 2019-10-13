a6a00fc3a592aa1b41fad34416547c7850780cc4
David <davidsatwork2@hotmail.com>
2019-10-13 17:47:09 +0800

    add styles to activities, change div to fragment

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import { List } from 'semantic-ui-react';
+import { List, Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 
@@ -14,14 +14,16 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <Fragment>
       <NavBar />
-      <List>
-        {activities.map(activity => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 };
 
