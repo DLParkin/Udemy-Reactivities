a0520935517846fdde1ac992955e62509025a2f1
David <davidsatwork2@hotmail.com>
2019-10-16 12:34:45 +0800

    add form to ActivityDashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivitiyDetails from '../details/ActivitiyDetails';
+import ActivityForm from '../form/ActivityForm';
 
 interface IProps {
   activities: IActivity[];
@@ -16,6 +17,7 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
       </Grid.Column>
       <Grid.Column width={6}>
         <ActivitiyDetails />
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
