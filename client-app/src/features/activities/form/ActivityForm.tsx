ca344ed1a31640d31e7acd4b921aab27e5d2802c
David <davidsatwork2@hotmail.com>
2019-10-16 12:36:37 +0800

    change desceiption to textarea, change date to type="date"

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,9 @@ const ActivityForm = () => {
     <Segment>
       <Form>
         <Form.Input placeholder='Title'/>
-        <Form.Input placeholder='Description'/>
+        <Form.TextArea rows={2} placeholder='Description'/>
         <Form.Input placeholder='Category'/>
-        <Form.Input placeholder='Date'/>
+        <Form.Input type="date" placeholder='Date'/>
         <Form.Input placeholder='City'/>
         <Form.Input placeholder='Venue'/>
       </Form>
