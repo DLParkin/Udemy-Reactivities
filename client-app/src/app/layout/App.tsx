87dced56779cffe4fa9b3c3834f5e1748a02a9f5
David <davidsatwork2@hotmail.com>
2019-10-19 12:24:45 +0800

    add loading activity to useffect for activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -51,8 +52,10 @@ const App = () => {
         activities.push(activity);
       });
       setActivities(activities);
-    });
+    }).then(() => setLoading(false));
   }, []);
+
+  if (loading) return <LoadingComponent content='Loading activities...'/>
 
   return (
     <Fragment>
