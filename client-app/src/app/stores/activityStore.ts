a272a73cec22136374764c865f6bc29248fc1cfd
David <davidsatwork2@hotmail.com>
2019-10-20 12:49:41 +0800

    add basic catch to loadActivities in activityStore

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -11,18 +11,21 @@ class ActivityStore {
 
   @action loadActivities = () => {
     this.loadingInitial = true;
-    agent.Activities.list().then(activities => {
-      activities.forEach(activity => {
-        activity.date = activity.date.split('.')[0];
-        this.activities.push(activity);
-      });
-    }).finally(() => this.loadingInitial = false);
-  }
+    agent.Activities.list()
+      .then(activities => {
+        activities.forEach(activity => {
+          activity.date = activity.date.split('.')[0];
+          this.activities.push(activity);
+        });
+      })
+      .catch(error => console.log(error))
+      .finally(() => (this.loadingInitial = false));
+  };
 
   @action selectActivity = (id: string) => {
     this.selectedActivity = this.activities.find(a => a.id === id);
     this.editMode = false;
-  }
+  };
 }
 
-export default createContext(new ActivityStore())
+export default createContext(new ActivityStore());
