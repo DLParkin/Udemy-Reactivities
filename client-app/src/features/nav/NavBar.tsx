fe1f29dd6dd5dc7682e98afbe195d728d6e31f5c
David <davidsatwork2@hotmail.com>
2019-10-24 10:32:00 +0800

    change to NavLink, highlights current tab

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -2,20 +2,20 @@ import React, { useContext } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../app/stores/activityStore';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   const activityStore = useContext(ActivityStore);
   return (
     <Menu fixed="top" inverted>
       <Container>
-        <Menu.Item header as={Link} to='/'>
+        <Menu.Item header as={NavLink} to='/'>
           <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
           Reactivities
         </Menu.Item>
-        <Menu.Item name="Activities" as={Link} to='/activities'/>
+        <Menu.Item name="Activities" as={NavLink} to='/activities'/>
         <Menu.Item>
-          <Button as={Link} to='/createActivity' positive content="Create Activity" />
+          <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
