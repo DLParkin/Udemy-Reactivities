c5e1910719cd228b3f1dc8511659e5f1c1dc6002
David <davidsatwork2@hotmail.com>
2019-10-24 10:19:02 +0800

    wrap App with BrowserRouter in index.tsx

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,7 +5,12 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
