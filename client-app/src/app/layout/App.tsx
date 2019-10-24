8299e211c9c00e2bcaf06fb74721b0e2e592d05b
David <davidsatwork2@hotmail.com>
2019-10-24 12:41:08 +0800

    add manage state for edit to app.tsx, add link to button in activityDetails

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -24,8 +24,8 @@ const App = () => {
       <Container style={{ marginTop: '7em' }}>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/activities" component={ActivityDashboard} />
-        <Route path='/activities/:id' component={ActivityDetails} />
-        <Route path="/createActivity" component={ActivityForm} />
+        <Route path="/activities/:id" component={ActivityDetails} />
+        <Route path={['/createActivity', '/manage/:id']} component={ActivityForm} />
       </Container>
     </Fragment>
   );
