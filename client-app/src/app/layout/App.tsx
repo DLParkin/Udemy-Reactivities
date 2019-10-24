d95c5840cd1b9cb7bc3bd21c92a496f4901bd9af
David <davidsatwork2@hotmail.com>
2019-10-24 16:04:07 +0800

    add withRouter to app componenet so we have access to location key from router

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -5,7 +5,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from './LoadingComponent';
 import ActivityStore from '../stores/activityStore';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
@@ -31,4 +31,4 @@ const App = () => {
   );
 };
 
-export default observer(App);
+export default withRouter(observer(App));
