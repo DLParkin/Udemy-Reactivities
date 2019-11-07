b86a082cc9fd918e09426c1dcb634234192469f1
David <davidsatwork2@hotmail.com>
2019-11-07 12:57:19 +0800

    create groupActivitiesByDate, readjust activitiesByDate to use new function

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,12 @@ class ActivityStore {
   @observable target = '';
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
-      (a, b) => Date.parse(a.date) - Date.parse(b.date)
-    );
+    return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+    return sortedActivities;
   }
 
   @action loadActivities = async () => {
@@ -39,7 +42,7 @@ class ActivityStore {
 
   @action clearActivity = () => {
     this.activity = null;
-  }
+  };
 
   @action loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
