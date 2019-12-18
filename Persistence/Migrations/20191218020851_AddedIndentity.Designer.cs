9b97a0a701de2f678144b5b13878756f1baeb3ae
David <davidsatwork2@hotmail.com>
2019-12-18 10:38:26 +0800

    added Idnetity to database, what a head ****

--- Persistence/Migrations/20191218020851_AddedIndentity.Designer.cs
+++ Persistence/Migrations/20191218020851_AddedIndentity.Designer.cs
@@ -9,14 +9,14 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20191218015816_addedIndentity")]
-    partial class addedIndentity
+    [Migration("20190803114948_AddedIdentity")]
+    partial class AddedIdentity
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
 
             modelBuilder.Entity("Domain.Activity", b =>
                 {
--- Persistence/Migrations/20191218020851_AddedIndentity.cs
+++ Persistence/Migrations/20191218020851_AddedIndentity.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Persistence.Migrations
 {
-    public partial class addedIndentity : Migration
+    public partial class AddedIdentity : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
