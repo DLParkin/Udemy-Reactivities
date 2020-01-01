2776de46401a6cb6c9b4913b80355bf4ab7a407c
David <davidsatwork2@hotmail.com>
2020-01-01 13:59:57 +0800

    return username

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -17,6 +17,8 @@ namespace Infrastructure.Security
         public string GetCurrentUsername()
         {
             var username = _httpContextAccessor.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return username;
         }
     }
 }
